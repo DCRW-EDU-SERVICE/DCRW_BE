53dbc5a7749a91c12614092aa23f17e76b5e1daf
hh830 <jhh8890@naver.com>
2024-08-09 00:02:40 +0900

    Init: spring boot init

---
+++ src/main/java/com/example/DCRW/DcrwApplication.java
@@ -0,0 +1,13 @@
+package com.example.DCRW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcrwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcrwApplication.class, args);
+	}
+
+}
