872797d4da1886549b5b55b46dfd6c983150d6ea
hh830 <jhh8890@naver.com>
2024-08-22 22:01:55 +0900

    feat: 생성자에서 builder 패턴 사용

--- src/main/java/com/example/DCRW/service/RegisterService.java
+++ src/main/java/com/example/DCRW/service/RegisterService.java
@@ -1,26 +1,23 @@
 package com.example.DCRW.service;
 
 import com.example.DCRW.dto.UserDto;
-import com.example.DCRW.entity.User;
-import com.example.DCRW.repository.UserRepository;
-import org.springframework.boot.autoconfigure.task.TaskSchedulingProperties;
+import com.example.DCRW.entity.Users;
+import com.example.DCRW.repository.UsersRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegisterService {
-    private final UserRepository userRepository;
+    private final UsersRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public RegisterService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public RegisterService(UsersRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    @Transactional
     public void Register(UserDto userDto){
         String userName = userDto.getUserId();
         String password = userDto.getPassword();
@@ -30,15 +27,13 @@ public class RegisterService {
             return;
         }
 
-        User data = new User();
-        data.setUserId(userName);
-        data.setPassword(bCryptPasswordEncoder.encode(password)); // 비밀번호 암호화
-
-        data.setName(userDto.getName());
-        data.setAddress(userDto.getAddress());
-
-        data.setBirthDate(userDto.getBirthDate());
-//        data.setJoinDate((Timestamp) userDto.getJoinDate());
+        Users data = Users.builder()
+                .userId(userName)
+                .password(bCryptPasswordEncoder.encode(password))
+                .name(userDto.getName())
+                .address(userDto.getAddress())
+                .birthDate(userDto.getBirthDate())
+                .build();
 
         userRepository.save(data);
     }
