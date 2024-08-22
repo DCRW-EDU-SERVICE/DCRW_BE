212c56d9d14661a03b7be7822b3f7b0817395ab8
hh830 <jhh8890@naver.com>
2024-08-22 22:01:29 +0900

    feat: users 엔티티 이름 변경

--- src/main/java/com/example/DCRW/repository/UsersRepository.java
+++ src/main/java/com/example/DCRW/repository/UsersRepository.java
@@ -1,12 +1,12 @@
 package com.example.DCRW.repository;
 
-import com.example.DCRW.entity.User;
-import org.hibernate.annotations.processing.SQL;
+import com.example.DCRW.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository // JpaRepository<엔티티, 엔티티의 id의 Reference Type>
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UsersRepository extends JpaRepository<Users, String> {
     Boolean existsByUserId(String username);
+
+    Users findByUserId(String username);
 }
