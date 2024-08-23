fee3bed5d74065821a3516637bed16ec283cd492
hh830 <jhh8890@naver.com>
2024-08-23 21:33:19 +0900

    feat: 엔티티 생성

---
+++ src/main/java/com/example/DCRW/entity/Notification.java
@@ -0,0 +1,33 @@
+package com.example.DCRW.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification {
+    @Id
+    @Column(name = "notification_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificationId;
+    private String content;
+    @Column(name = "notification_date")
+    private Timestamp notificationDate;
+    @Column(name = "read_status")
+    private int readStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @Builder
+    public Notification(String content, Timestamp notificationDate, int readStatus) {
+        this.content = content;
+        this.notificationDate = notificationDate;
+        this.readStatus = readStatus;
+    }
+}
