e9e95187220b8a1298d236a9d0eb86f7d9204044
hh830 <jhh8890@naver.com>
2024-09-04 14:39:10 +0900

    feat: 예외 핸들러 추가

--- src/main/java/com/example/DCRW/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/DCRW/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -37,21 +38,56 @@ public class GlobalExceptionHandler {
 //        fieldErrors.forEach(error ->
 //                errors.put(error.getField(), error.getDefaultMessage())
 //        );
+        ex.printStackTrace();
 
         return new ResponseEntity<>(resultDto, HttpStatus.BAD_REQUEST);
     }
 
-    // 사용자 찾을 수 없음
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<ResultDto<String>> handleResponseStatusException(ResponseStatusException ex) {
-        return new ResponseEntity<>(setDto(HttpStatus.NOT_FOUND, ex.getMessage()), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ResultDto<String>> handleNoSuchElementException(NoSuchElementException ex) {
+        ResultDto<String> resultDto = ResultDto.<String>builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message("요청한 사용자를 찾을 수 없습니다: " + ex.getMessage())
+                .build();
+        ex.printStackTrace();
+        return new ResponseEntity<>(resultDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ResultDto<String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ResultDto<String> resultDto = ResultDto.<String>builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("잘못된 요청: " + ex.getMessage())
+                .build();
+        ex.printStackTrace();
+
+        return new ResponseEntity<>(resultDto, HttpStatus.BAD_REQUEST);
     }
 
     // 그 외 예외 처리
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ResultDto<String>> handleRuntimeException(RuntimeException ex) {
+        ResultDto<String> resultDto = ResultDto.<String>builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message("오류 발생: " + ex.getMessage())
+                .build();
+        ex.printStackTrace();
+
+        return new ResponseEntity<>(resultDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResultDto<String>> handleGenericException(Exception ex) {
-        return new ResponseEntity<>(setDto(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        ResultDto<String> resultDto = ResultDto.<String>builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message("exception 오류 발생: " + ex.getMessage())
+                .build();
+        ex.printStackTrace();
+
+        return new ResponseEntity<>(resultDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
 
     // 기본 예외 응답 dto (data 없음)
     private ResultDto setDto(HttpStatus httpStatus, String message){
