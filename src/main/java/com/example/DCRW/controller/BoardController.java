7590dc7ba3c75cf5abbbe37d5fac84c7944c8c2a
hh830 <jhh8890@naver.com>
2024-09-06 22:34:49 +0900

    feat: 게시글 등록, 수정, 삭제

--- src/main/java/com/example/DCRW/controller/BoardController.java
+++ src/main/java/com/example/DCRW/controller/BoardController.java
@@ -1,10 +1,10 @@
 package com.example.DCRW.controller;
 
 import com.example.DCRW.dto.ResultDto;
-import com.example.DCRW.dto.board.PostDetailDto;
-import com.example.DCRW.dto.board.PostPageDto;
-import com.example.DCRW.dto.board.PostResponseDto;
+import com.example.DCRW.dto.board.*;
+import com.example.DCRW.entity.File;
 import com.example.DCRW.service.board.BoardService;
+import com.example.DCRW.service.board.FileService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -13,19 +13,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/boards")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+    private final FileService fileService;
 
     // logger
 //    private final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
     // 전체, 독서, 한글, 부모 게시판 전체 조회
     @GetMapping
-    public ResponseEntity<ResultDto<Object>> showBoards(@RequestParam("type") int boardType, @RequestParam("page") int page, @RequestParam("size") int size){
+    public ResponseEntity<ResultDto<Object>> showBoards(@RequestParam("type") int boardType, @RequestParam("page") int page, @RequestParam("size") int size) {
         Page<PostResponseDto> postResponseDto = boardService.showPost(boardType, page, size);
 
         ResultDto<Object> resultDto = ResultDto.builder()
@@ -83,7 +89,7 @@ public class BoardController {
 
     // 게시글 세부 조회
     @GetMapping("/posts/{postID}")
-    public ResponseEntity<ResultDto<Object>> detailPost(@PathVariable("postID") int postId){
+    public ResponseEntity<ResultDto<Object>> detailPost(@PathVariable("postID") int postId) {
         PostDetailDto detailDtos = boardService.detailPost(postId);
 
         ResultDto<Object> resultDto = ResultDto.builder()
@@ -95,21 +101,63 @@ public class BoardController {
         return new ResponseEntity<>(resultDto, HttpStatus.OK);
     }
 
-    // 게시글 등록
+    // 게시글 등록 -> content-type이 multipart/form-data
     @PostMapping("{boardType}/posts")
-    public ResponseEntity<ResultDto<Object>> addPosts(){
-        return null;
+    public ResponseEntity<ResultDto<Object>> addPosts(
+            @RequestPart("post") PostAddDto postAddDto,
+            @RequestPart(value = "file", required = false) List<MultipartFile> files) {
+
+        List<File> fileList = new ArrayList<>();
+
+        if (files != null && !files.isEmpty()) {
+            fileList = fileService.settingFile(files);
+        }
+
+        int postId = boardService.addPosts(postAddDto, fileList); // post DB 저장
+
+        if (files != null && !files.isEmpty()) {
+            fileService.saveFile(files, fileList); // 파일 저장
+        }
+
+        // 파일 저장 실패 시 데이터베이스 롤백
+        boardService.removePost(postId);
+
+        ResultDto resultDto = ResultDto.builder()
+                .status(HttpStatus.OK)
+                .message("게시글 등록 성공")
+                .build();
+
+        return new ResponseEntity<>(resultDto, HttpStatus.OK);
     }
 
     // 게시글 수정
     @PatchMapping("{boardType}/posts/{postID}")
-    public ResponseEntity<ResultDto<Object>> updatePost(){
-        return null;
+    public ResponseEntity<ResultDto<Object>> updatePost(
+            @PathVariable("boardType") int boardType,
+            @PathVariable("postID") int postId,
+            @RequestPart("post")PostUpdateDto postUpdateDto,
+            @RequestPart(value = "file", required = false) List<MultipartFile> fileAddList) {
+
+        boardService.updatePost(postId, postUpdateDto, fileAddList);
+
+        ResultDto resultDto = ResultDto.builder()
+                .status(HttpStatus.OK)
+                .message("게시글 수정 성공")
+                .build();
+
+        return new ResponseEntity<>(resultDto, HttpStatus.OK);
     }
 
     // 게시글 삭제
     @DeleteMapping("{boardType}/posts/{postID}")
-    public ResponseEntity<ResultDto<Object>> deletePost(){
-        return null;
+    public ResponseEntity<ResultDto<Object>> deletePost(@PathVariable("boardType") int boardType, @PathVariable("postID") int postId) {
+        boardService.removePost(postId);
+
+        ResultDto resultDto = ResultDto.builder()
+                .status(HttpStatus.OK)
+                .message("게시글 삭제 성공")
+                .build();
+
+        return new ResponseEntity<>(resultDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/DCRW/service/board/BoardService.java
+++ src/main/java/com/example/DCRW/service/board/BoardService.java
@@ -1,22 +1,37 @@
 package com.example.DCRW.service.board;
 
+import com.example.DCRW.dto.board.PostAddDto;
 import com.example.DCRW.dto.board.PostDetailDto;
 import com.example.DCRW.dto.board.PostResponseDto;
-import com.example.DCRW.entity.Board;
-import com.example.DCRW.entity.Category;
-import com.example.DCRW.entity.Post;
-import com.example.DCRW.repository.BoardRepository;
-import com.example.DCRW.repository.CategoryRepository;
-import com.example.DCRW.repository.PostRepository;
+import com.example.DCRW.dto.board.PostUpdateDto;
+import com.example.DCRW.dto.user.CustomUserDetails;
+import com.example.DCRW.entity.*;
+import com.example.DCRW.repository.*;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +39,9 @@ public class BoardService {
     private final PostRepository postRepository;
     private final BoardRepository boardRepository;
     private final CategoryRepository categoryRepository;
+    private final UsersRepository usersRepository;
+    private final FileService fileService;
+    private final FileRepository fileRepository;
 
     // 전체
     public Page<PostResponseDto> showPost(int boardType, int page, int size) {
@@ -35,11 +53,10 @@ public class BoardService {
 
             return postRepository.showPost(board, pageable);
 
-        } catch (DataAccessException de){
+        } catch (DataAccessException de) {
             de.printStackTrace();
             throw new RuntimeException("데이터베이스 접근 중 오류");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("게시글 조회에 오류가 발생했습니다");
         }
     }
@@ -58,11 +75,10 @@ public class BoardService {
 
             return postRepository.searchPostCategory(board, pageable, category);
 
-        } catch (DataAccessException de){
+        } catch (DataAccessException de) {
             de.printStackTrace();
             throw new RuntimeException("데이터베이스 접근 중 오류");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("게시글 조회에 오류가 발생했습니다");
         }
     }
@@ -75,15 +91,14 @@ public class BoardService {
             Board board = boardRepository.findById(boardType)
                     .orElseThrow(() -> new IllegalArgumentException("Board Type이 존재하지 않습니다"));
 
-            query = "%"+query+"%";
+            query = "%" + query + "%";
 
             return postRepository.searchPostTitle(board, pageable, query);
 
-        } catch (DataAccessException de){
+        } catch (DataAccessException de) {
             de.printStackTrace();
             throw new RuntimeException("데이터베이스 접근 중 오류");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("게시글 조회에 오류가 발생했습니다");
         }
     }
@@ -99,36 +114,164 @@ public class BoardService {
             Category category = categoryRepository.findById(categoryId)
                     .orElseThrow(() -> new IllegalArgumentException("category type이 존재하지 않습니다"));
 
-            query = "%"+query+"%";
+            query = "%" + query + "%";
 
             return postRepository.searchPostTitleCategory(board, pageable, category, query);
 
-        } catch (DataAccessException de){
+        } catch (DataAccessException de) {
             de.printStackTrace();
             throw new RuntimeException("데이터베이스 접근 중 오류");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("게시글 조회에 오류가 발생했습니다");
         }
     }
 
     // 게시글 상세 조회
     public PostDetailDto detailPost(int postId) {
-        try{
+        try {
             Post post = postRepository.findById(postId)
                     .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다"));
+
+            // 파일 정보를 FileDto 리스트로 변환
+            List<File> fileList = post.getFileList().stream()
+                    .map(file -> File.builder()
+                            .fileName(file.getFileName())
+                            .filePath(file.getFilePath())  // S3 URL 또는 서버 경로
+                            .build())
+                    .collect(Collectors.toList());
 
             PostDetailDto postDetailDto = PostDetailDto.builder()
                     .title(post.getTitle())
                     .content(post.getContent())
                     .category(post.getCategory().getCategoryId())
-                    .file(post.getFileList())
+                    .file(fileList)  // 변환된 파일 리스트 추가
                     .build();
 
             return postDetailDto;
 
-        } catch (Exception e){
-            throw new RuntimeException("게시글 상세 조회에 오류가 발생했습니다");
+        } catch (Exception e) {
+            throw new RuntimeException("게시글 상세 조회에 오류가 발생했습니다 " + e.getMessage());
         }
     }
+
+
+    @Transactional
+    public int addPosts(PostAddDto postAddDto, List<File> files) {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+            Users users = usersRepository.findById(customUserDetails.getUsername())
+                    .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다"));
+
+            Board board = boardRepository.findById(postAddDto.getBoardId())
+                    .orElseThrow(() -> new IllegalArgumentException("게시판이 존재하지 않습니다"));
+
+            Category category = categoryRepository.findById(postAddDto.getCategory())
+                    .orElseThrow(() -> new IllegalArgumentException("카테고리가 존재하지 않습니다"));
+
+
+            Post post = Post.builder()
+                    .title(postAddDto.getTitle())
+                    .content(postAddDto.getContent())
+                    .users(users)
+                    .board(board)
+                    .category(category)
+                    .postDate(LocalDateTime.now())
+                    .fileList(files)
+                    .build();
+
+            for (File file : files) {
+                file.setPost(post);
+            }
+
+            int postId = postRepository.save(post).getPostId();
+
+            return postId;
+        } catch (Exception e) {
+            throw new RuntimeException("게시글 업로드에 실패했습니다");
+        }
+
+    }
+
+
+    // 삭제
+    @Transactional
+    public void removePost(int postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid post ID"));
+
+        List<File> files = post.getFileList();
+
+        // 데이터베이스에서 게시글 삭제
+        postRepository.delete(post);
+
+        // 트랜잭션 성공 후 파일을 비동기로 삭제
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+            @Override
+            public void afterCommit() {
+                try {
+                    fileService.deleteFilesAfterCommit(files);  // 비동기 파일 삭제 처리
+                } catch (IOException e) {
+                    throw new RuntimeException("파일 삭제에 오류가 발생했습니다 " + e.getMessage());
+                }
+            }
+        });
+    }
+
+
+    @Transactional
+    public void updatePost(int postId, PostUpdateDto postUpdateDto, List<MultipartFile> filesToAdd) {
+
+        // 게시글 조회
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+        try {
+            postUpdateDto.getTitle().ifPresent(post::setTitle); // title 값이 있을 때만 업데이트
+            postUpdateDto.getContent().ifPresent(post::setContent); // content 값이 있을 때만 업데이트
+
+            postRepository.save(post); // 변경된 엔티티 저장
+        } catch (Exception e){
+            throw new RuntimeException("제목, 내용 수정에 오류가 발생했습니다. " + e.getMessage());
+        }
+
+        try {
+            List<File> deleteList = new ArrayList<>();
+            // 파일 삭제 요청 있는 경우
+            if (postUpdateDto.getFileDelete() != null) {
+                for (int fileId : postUpdateDto.getFileDelete()) {
+                    // 파일 유무 확인
+                    File file = fileRepository.findById(fileId)
+                            .orElseThrow(() -> new IllegalArgumentException("파일을 찾을 수 없어요"));
+
+                    deleteList.add(file);
+                    fileRepository.delete(file); // 파일 db에서 삭제
+                }
+                fileService.deleteFilesAfterCommit(deleteList);
+            }
+
+            // 파일 추가 요청 있는 경우
+            List<File> fileList;
+
+            if (filesToAdd != null && !filesToAdd.isEmpty()) {
+                // 파일 엔티티 리스트 세팅
+                fileList = fileService.settingFile(filesToAdd);
+
+                for(File file : fileList){
+                    System.out.println("=============== "+file.getFileType());
+                    // post 설정
+                    file.setPost(post);
+                }
+                // file 엔티티 저장
+                fileRepository.saveAll(fileList);
+
+                // 서버에 파일 저장
+                fileService.saveFile(filesToAdd, fileList);
+            }
+        } catch (Exception e){
+
+            throw new RuntimeException("파일 업로드에 오류가 발생했습니다." + e.getMessage());
+        }
+    }
+
 }
