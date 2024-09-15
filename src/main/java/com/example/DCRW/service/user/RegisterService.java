3ceb0c5cd57e11f1682beea69c61696f041bbdf1
hh830 <jhh8890@naver.com>
2024-09-15 20:27:16 +0900

    feat: 교사 코드 등록

--- src/main/java/com/example/DCRW/service/user/RegisterService.java
+++ src/main/java/com/example/DCRW/service/user/RegisterService.java
@@ -2,6 +2,7 @@ package com.example.DCRW.service.user;
 
 import com.example.DCRW.dto.ResultDto;
 import com.example.DCRW.dto.user.RegisterDto;
+import com.example.DCRW.dto.user.TeacherDto;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface RegisterService {
@@ -10,4 +11,6 @@ public interface RegisterService {
 
     @Transactional(readOnly = true)
     boolean checkUsernameDuplication(String username);
+
+    void updateTeacher(TeacherDto teacherDto, String username);
 }
